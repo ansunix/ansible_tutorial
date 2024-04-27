6b75513ab9357835b74539ef28ee11037c96f20a
William Jackson <slipstreammm@gmail.com>
2024-04-27 09:31:58 -0700

    Added new function

--- hello.py
+++ hello.py
@@ -6,5 +6,10 @@ name = input ("what is your name? ")
 #print ("Moving Foward Intentionally.")
 
 # Say hello to user with a coma to add multipule variables with spaces added by using coma operator.
-print ("Hello," , name )
-print ("Believing is Seeing")
+#print ("Hello," , name )
+#print ("Believing is Seeing")
+
+# Say hello and do not go to a new line by using the end perameter, to only new line after second print function.
+print ("Hello, " , end="")
+print (name)
+print ("Will it into existence")

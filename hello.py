098ecea159b88aa7d68f45adbbc4bc7a36b0637e
William Jackson <slipstreammm@gmail.com>
2024-04-27 09:15:15 -0700

    updated comments

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 # Ask user for there name.
 name = input ("what is your name? ")
 
-# Say hello then concatenate with the plus symbol ti the value of name.
+# Say hello then concatenate with the plus operator with the value of name into a string.
 #print ("Hello, " + name )
 #print ("Moving Foward Intentionally.")
 
-# Say hello to user with a coma to add multipule variables with spaces added by using coma function.
+# Say hello to user with a coma to add multipule variables with spaces added by using coma operator.
 print ("Hello," , name )
 print ("Believing is Seeing")

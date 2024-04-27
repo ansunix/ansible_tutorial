5606cbe5ea498151889923468550926bd0302473
William Jackson <slipstreammm@gmail.com>
2024-04-27 09:11:06 -0700

    Added variables to script.

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 # Ask user for there name.
 name = input ("what is your name? ")
-# Say hello to user.
-print ("Hello,")
-print (name)
 
+# Say hello then concatenate with the plus symbol ti the value of name.
+#print ("Hello, " + name )
+#print ("Moving Foward Intentionally.")
+
+# Say hello to user with a coma to add multipule variables with spaces added by using coma function.
+print ("Hello," , name )
+print ("Believing is Seeing")
